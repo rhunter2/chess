94545d2481f3f9fc929b02f55a1dd8ace6c19603
rhunter2 <67162907+rhunter2@users.noreply.github.com>
2024-09-24 21:33:31 -0600

    built addPiece function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -16,29 +16,25 @@ public class ChessBoard {
     }
 
     /**
-     * Adds a chess piece to the chessboard
-     *
-     * @param position where to add the piece to
+     * Adds a chess piece to the chessboard * @param position where to add the piece to
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        if (position != null && piece != null && isValidPosition(position)) {
+            board.put(position,piece);
+        }
     }
 
     /**
      * Gets a chess piece on the chessboard
-     *
-     * @param position The position to get the piece from
-     * @return Either the piece at the position, or null if no piece is at that
-     * position
+     * @param position The position to get the piece from @return Either the piece at the position, or null if no piece is at that position
      */
     public ChessPiece getPiece(ChessPosition position) {
         throw new RuntimeException("Not implemented");
     }
 
     /**
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
+     * Sets the board to the default starting board (How the game of chess normally starts)
      */
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
