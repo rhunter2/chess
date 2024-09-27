9444eb4a5d78bcdd0d7941152195461c4875cd8d
rhunter2 <67162907+rhunter2@users.noreply.github.com>
2024-09-26 21:01:59 -0600

    some changes

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -21,7 +21,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -29,6 +29,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return column;
     }
 }
